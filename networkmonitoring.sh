bb90adde57017eda9530d8ee0b882854208bb90b
Tobias Hargesheimer <tobiash87@gmail.com>
2018-04-23 00:16:27 +0200

    fix %

--- networkmonitoring.sh
+++ networkmonitoring.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # 
 # This script checks the online connection every $waittime seconds
-# with a ping to $targetpingdevice and attempts to re-login if 
-# ping fails
+# with a ping to $HOSTS and attempts to re-login if ping fails
 #
 # Dependency: login.sh
 #
+
 
 
 waittime=30                     # insert time to wait between checks here
@@ -13,14 +13,16 @@ countpings=4					# insert the number of ping attempts
 
 HOSTS='134.93.178.2 google.de'	#uni-mainz.de&google.d	# add ip / hostname separated by white space
 
-let COUNTER=0 # Do NOT edit!
-let ERROR=0 # Do NOT edit!
+
 
 while true; do
 	echo "Check start at $(date)"
+	
+	let COUNTER=0 
+	let ERROR=0 
+	
 	for HOST in $HOSTS
 	do
-		#sleep 5
 		# check packet loss in %
 		pingvalue=$(ping -qc $countpings $HOST | grep -oP '\d+(?=% packet loss)')
 		if [ $pingvalue -ge 50 ]; then
@@ -29,13 +31,17 @@ while true; do
 		fi
 		let COUNTER=COUNTER+1
 	done
+	
 	# login again, when more then 50% pings to hosts failed
-	if [ $COUNTER -eq $ERROR ]; then
+	#let ERROR_Percent=$((200*$ERROR/$COUNTER % 2 + 100*$ERROR/$COUNTER))
+	let ERROR_Percent=$((100*$ERROR/$COUNTER))
+	printf "%.2f%% ping failed\n" $ERROR_Percent
+	if [ $ERROR_Percent -gt 50 ]; then
 		echo "Logging in... at $(date)"
 		./login_wohnheim_uni_mainz_de.sh login > /dev/null
 		continue
 	fi
-	let COUNTER=0
-	let ERROR=0
+	
     sleep $waittime
+	
 done;
